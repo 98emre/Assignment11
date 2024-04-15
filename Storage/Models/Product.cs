61e29e2f1e8b5865133aeb36034471001fc4491f
98emre <emre-98@live.se>
2024-04-16 00:11:03 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int Id {  get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0,1000000)]
+        public int Price { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Orderdate { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string Category { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 1)]
+        public string Shelf { get; set; }
+
+        [Range(0,1000)]
+        public int Count { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+
+    }
+}
---
+++ Storage/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int InventoryValue { get; set; }
+    }
+}
